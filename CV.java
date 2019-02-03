44b59ac58d1c778503d561f998c3680bc7b83271
Antoine Martineau <martineau.antoin3@gmail.com>
2019-02-03 02:12:03 -0500

    Ajout des attributs de la classe CV

---
+++ CV.java
@@ -0,0 +1,10 @@
+public class CV {
+	Private String nom;
+	Private String prenom;
+	Private String formation;
+	Private int experience;
+	Private String[] competence;
+	Private String  attente;
+	
+	
+}
