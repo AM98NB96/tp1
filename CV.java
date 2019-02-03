9551159ecc183eeff4cf3dc6859ad4079a05d74e
Antoine Martineau <martineau.antoin3@gmail.com>
2019-02-03 02:36:30 -0500

    Création de la méthode main de la classe CV

--- CV.java
+++ CV.java
@@ -20,4 +20,14 @@ public class CV {
         }
         System.out.println("\nAttentes: " + this.Attentes + "\n");
 	}
+	
+	public void main() {
+		CV cvNicolas = new CV("Barrette", "Nicolas", "CFP des riverains", 3, Arrays.asList("Programmation", "Soutien technique"), "Passer le cours");
+        CV cvAntoine = new CV("Martineau", "Antoine", "CFP des riverains", 1, Arrays.asList("Programmation", "Soutien technique"), "Apprendre à faire un logiciel de qualité.");
+		
+		System.out.println("Bienvenue chez Barrette!");
+		
+		cvNicolas.affiche();
+		cvAntoine.affiche();
+	}
 }
