ead72264c68a760d89e5ff4dcfdf1494de7651e1
Antoine Martineau <martineau.antoin3@gmail.com>
2019-02-03 02:19:08 -0500

    Création de la méthode affiche de la classe CV

--- CV.java
+++ CV.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
 public class CV {
+	
 	Private String nom;
 	Private String prenom;
 	Private String formation;
 	Private int experience;
-	Private String[] competence;
+	List<String> competence;
 	Private String  attente;
 	
-	
+	public void affiche() {
+		System.out.print("Nom: " + this.nom
+                + "\nPrenom: " + this.prenom
+                + "\nFormation: " + this.formation
+                + "\nExperience: " + this.experience
+                + "\nCompetences: ");
+        for (String competence : Competences) {
+            System.out.print("\n" + competence);
+        }
+        System.out.println("\nAttentes: " + this.Attentes + "\n");
+	}
 }
