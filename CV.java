1f0995d855fca78a2d9578136b747fa1a69c60d6
Antoine Martineau <martineau.antoin3@gmail.com>
2019-02-11 17:00:55 -0500

    Ajout de style.txt contenant les normes de programmation et application des normes dans CV.java.

--- CV.java
+++ CV.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
+/**
+ * La classe CV contient les informations du CV des usagers.
+ * 
+ * @param nom
+ * 			Nom de l'usager.
+ * @param prenom
+ * 			Prenom de l'usager.
+ * @param formation
+ * 			Formation de l'usager.
+ * @param experience
+ * 			Experience de l'usager.
+ * @param competence
+ * 			Competence de l'usager.
+ * @param attente
+ * 			Attente de l'usager vis-à-vis le cours.
+ */
+
 public class CV {
 	
 	Private String nom;
@@ -9,6 +26,9 @@ public class CV {
 	List<String> competence;
 	Private String  attente;
 	
+	/**
+	 * Constructeur de la classe CV.
+	 */
 	
 	public CV(String nom, String prenom, String formation, int experience, List<String> competences, String attentes) {
         this.nom = nom;
@@ -18,6 +38,10 @@ public class CV {
         this.competences = competences;
         this.attentes = attentes;
     }
+	
+	/**
+	 * La méthode affiche permet d'afficher les informations de l'usager.
+	 */
 	
 	public void affiche() {
 		System.out.print("Nom: " + this.nom
@@ -31,9 +55,14 @@ public class CV {
         System.out.println("\nAttentes: " + this.Attentes + "\n");
 	}
 	
+	/**
+	 * La méthode main créé des objets de type CV, affiche un message de bienvenue et les objets créés.
+	 */
+	
 	public void main() {
 		CV cvNicolas = new CV("Barrette", "Nicolas", "CFP des riverains", 3, Arrays.asList("Programmation", "Soutien technique"), "Passer le cours");
-        CV cvAntoine = new CV("Martineau", "Antoine", "CFP des riverains", 1, Arrays.asList("Programmation", "Soutien technique"), "Apprendre à faire un logiciel de qualité.");
+        CV cvAntoine = new CV("Martineau", "Antoine", "CFP des riverains", 1, Arrays.asList("Programmation", "Soutien technique"),
+        					  "Apprendre à faire un logiciel de qualité.");
 		
 		System.out.println("Bienvenue chez Barrette!");
 		
