bf94ad63b7d4bd1789614b061f756b317bbe7d67
Antoine Martineau <martineau.antoin3@gmail.com>
2019-02-11 18:15:10 -0500

    Ajout de la classe Plat

---
+++ Plat.java
@@ -0,0 +1,27 @@
+package TP1;
+/**
+ * La classe Plat contient les informations du plat.
+ * @param nom
+ * 		Le nom du plat.
+ * @param cout
+ * 		Le cout du plat.
+ */
+public class Plat {
+	
+	public String nom;
+	public double cout;
+	
+	/**
+	 * Constructeur avec paramêtre de la classe plat
+	 * 
+	 * @param nom
+	 * @param cout
+	 * 
+	 */
+	public Plat(String nom, double cout) {
+		this.nom = nom;
+		this.cout = cout;
+	}
+	
+	
+}
