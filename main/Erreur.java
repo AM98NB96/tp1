7b31a3b73b3b88531a384cbf88487aec00667c5c
Nicolas Barrette <Nicolas.Barrette@live.ca>
2019-03-19 15:10:50 -0400

    Ajout de la classe Erreur ainsi qu'une liste d'objet Erreur dans la classe Main

--- main/Erreur.java
+++ main/Erreur.java
@@ -2,4 +2,8 @@ package main;
 
 public class Erreur {
 
+	public Erreur(String commande, String erreur) {
+		
+	}
+	
 }
--- main/Main.java
+++ main/Main.java
@@ -19,6 +19,7 @@ public class Main {
 	public static LinkedList<Client> clients = new LinkedList<Client>();
 	public static LinkedList<Plat> plats = new LinkedList<Plat>();
 	public static LinkedList<Commande> commandes = new LinkedList<Commande>();
+	public static LinkedList<Erreur> erreurs = new LinkedList<Erreur>();
 	
 	public static void main(String[] args) {
 	
