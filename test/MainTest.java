d9cd3087d5c8ccab44ae1aae3a5c991d1bbf24b6
Nicolas Barrette <Nicolas.Barrette@live.ca>
2019-03-19 15:14:33 -0400

    ajout des tests des méthodes LireFichierObjet de la classe Main

--- test/MainTest.java
+++ test/MainTest.java
@@ -2,13 +2,17 @@ package test;
 
 import main.*;
 
+
 import static org.junit.Assert.assertEquals;
 
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 public class MainTest {
 	
 	@Before
@@ -16,8 +20,12 @@ public class MainTest {
 		Main.clients = new LinkedList<Client>();
 		Main.plats = new LinkedList<Plat>();
 		Main.commandes = new LinkedList<Commande>();
+		Main.erreurs = new LinkedList<Erreur>();
 	}
 	
+	/* ***************************************************
+	 * Test methode CreerFacture()
+	 *************************************************** */
 	@Test
 	public void testCreerFacture() {
 		
@@ -139,7 +147,85 @@ public class MainTest {
 													   "TVQ(10%):            1.00$\r\n" + 
 											 		   "Total:              11.50$");			
 	}
+
+	/* ***************************************************
+	 * Test methode LireFichierObjet()
+	 *************************************************** */
 	
+	// Test d'existance
+	public void testLireFichierObjetClientExistePas() {
+		Main.lireFichierObjet("testClientExistePas.txt");
+		Erreur erreur = new Erreur("Joe Poutine 1", "Le client n'existe pas.");
+		assertEquals(Main.erreurs.get(0), erreur);
+	}
+	
+	public void testLireFichierObjetPlatExistePas() {
+		Main.lireFichierObjet("testPlatExistePas.txt");
+		
+		Erreur erreur = new Erreur("Roger Poulet 1", "Le plat n'existe pas.");
+		
+		assertEquals(Main.erreurs.get(0), erreur);
+	}
+
+	// Test de formats
+	public void testLireFichierObjetFormatClient() {
+		
+		Main.lireFichierObjet("testFormatClient.txt");
+		
+		Erreur erreur = new Erreur("Roger Dulac", "Le format du client est invalide, car il ne doit pas contenir d'espaces.");
+		Erreur erreur1 = new Erreur("Robert?", "Le format du client est invalide, car il ne doit pas contenir de caracteres spéciaux autre que '-'.");
+		Erreur erreur2 = new Erreur("Regeant123", "Le format du client est invalide, car il ne doit pas contenir de caracteres numériques.");
+		
+		assertEquals(Main.erreurs.get(0), erreur);
+		assertEquals(Main.erreurs.get(1), erreur1);
+		assertEquals(Main.erreurs.get(2), erreur2);
+	}
+	
+	public void testLireFichierObjetFormatPlat(){
+		
+		Main.lireFichierObjet("testFormatPlat.txt");
+		
+		Erreur erreur = new Erreur("Poutine", "Le format du plat est invalide, car il manque un paramêtre.");
+		Erreur erreur1 = new Erreur("Poutine blah", "Le format du plat est invalide, car blah n'est pas un prix.");
+		
+		assertEquals(Main.erreurs.get(0), erreur);
+		assertEquals(Main.erreurs.get(1), erreur1);
+		
+	}
+	
+	public void testLireFichierObjetFormatCommande(){
+		
+		Main.lireFichierObjet("testFormatCommande.txt");
+		
+		Erreur erreur = new Erreur("Roger Poutine", "Le format des la commande est invalide, car il manque un paramêtre.");
+		Erreur erreur1 = new Erreur("Roger Poutine blah", "Le format des la commande est invalide, car blah n'est pas une quantité.");
+		Erreur erreur2 = new Erreur("Roger Poutine 0.5", "Le format des la commande est invalide, car 0.5 n'est pas une quantité.");
+		
+		assertEquals(Main.erreurs.get(0), erreur);
+		assertEquals(Main.erreurs.get(1), erreur1);
+		assertEquals(Main.erreurs.get(2), erreur2);
+	}
+	// Test de validité
+	public void testLireFichierObjetPrixInvalide() {
+		
+		Main.lireFichierObjet("testPrixInvalide.txt");
+		
+		Erreur erreur = new Erreur("Poutine -3", "Le prix du plat ne peut pas être égal ou inférieur à 0.");
+		
+		assertEquals(Main.erreurs.get(0), erreur);
+	}
+	
+	public void testLireFichierObjetQuantiteInvalide() {
+		
+		Main.lireFichierObjet("testQuantiteInvalide.txt");
+		
+		Erreur erreur = new Erreur("Roger Poutine -1", "La quantité ne peut pas être inférieure à 0.");
+		Erreur erreur1 = new Erreur("Roger Poutine 0", "la quantité ne peut pas être 0.");
+		
+		assertEquals(Main.erreurs.get(0), erreur);
+		assertEquals(Main.erreurs.get(1), erreur1);
+	}
+
 	@After
 	public void apresChaqueTest() {
 		Main.clients = null;
