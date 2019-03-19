3df7edf298236ab549274b0db4094846a6a01cea
Nicolas Barrette <Nicolas.Barrette@live.ca>
2019-03-19 18:34:56 -0400

    creation des méthodes creerFacture et assemblerFacture qui permettent la création des factures.

--- main/Main.java
+++ main/Main.java
@@ -1,5 +1,7 @@
 package main;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -22,7 +24,6 @@ public class Main {
 	public static LinkedList<Erreur> erreurs = new LinkedList<Erreur>();
 	
 	public static void main(String[] args) {
-	
 		try {
 			lireFichierObjet("test.txt");
 		}catch (Exception e) {
@@ -30,7 +31,7 @@ public class Main {
 		}
 		
 		try {
-			creerFacture("facture.txt");
+			creerFichierFacture("facture.txt");
 		} catch (Exception e) {
 			System.out.println("erreur");
 		}
@@ -42,71 +43,117 @@ public class Main {
 	 * 			Contient le nom du fichier
 	 * @throws FileNotFoundException
 	 */
-	public static void lireFichierObjet(String fichier) throws FileNotFoundException {
+	public static void lireFichierObjet(String fichier) {
+		try {
+			Scanner input = new Scanner(new FileReader(fichier));
+			String typeObj = "";
+			while (input.hasNextLine()) {
+				String tmpString = input.nextLine();
+					//Type d'objet courrant
+				if (tmpString.equals("Clients :") || tmpString.equals("Plats :") || tmpString.equals("Commandes :") || tmpString.equals("Fin")) {
+					typeObj = tmpString;
+				}else{
+					
+					//Séparation des lignes du fichier d'objets
+					String[] tmp = tmpString.split(" ");
+					
+					switch (typeObj) {
+					
+						//Création des clients
+						case "Clients :":
+							clients.add(new Client(tmpString));					
+							break;
+							
+						//Création des plats
+						case "Plats :":
+							plats.add(new Plat(tmp[0], Double.parseDouble(tmp[1])));
+							break;
 		
-		Scanner input = new Scanner(new FileReader(fichier));
-		
-		String typeObj = "";
-		
-		while (input.hasNextLine()) {
-			
-			String tmpString = input.nextLine();
-				//Type d'objet courrant
-			if (tmpString.equals("Clients :") || tmpString.equals("Plats :") || tmpString.equals("Commandes :") || tmpString.equals("Fin")) {
-				
-				typeObj = tmpString;
-				
-			}else{
-				
-				//Séparation des lignes du fichier d'objets
-				String[] tmp = tmpString.split(" ");
-				
-				switch (typeObj) {
-				
-					//Création des clients
-					case "Clients :":
-						clients.add(new Client(tmpString));					
-						break;
-						
-					//Création des plats
-					case "Plats :":
-						plats.add(new Plat(tmp[0], Double.parseDouble(tmp[1])));
-						break;
-	
-					//Création des commandes
-					case "Commandes :":
-						
-						Client client = new Client();
-						Plat plat = new Plat();
-						int qte;
-						
-						for (Client c : clients) {
-							client = (c.nom.equals(tmp[0]) ? c : client);
-						}
-						for (Plat p : plats) {
-							plat = (p.nom.equals(tmp[1]) ? p : plat);
-						}
-						qte = Integer.parseInt(tmp[2]);
-						
-						commandes.add(new Commande(client, plat, qte));
-						
-						break;
-					case "Fin":
-						// Fin de la lecture
-						break;
+						//Création des commandes
+						case "Commandes :":
+							
+							Client client = new Client();
+							Plat plat = new Plat();
+							int qte;
+							
+							for (Client c : clients) {
+								client = (c.nom.equals(tmp[0]) ? c : client);
+							}
+							for (Plat p : plats) {
+								plat = (p.nom.equals(tmp[1]) ? p : plat);
+							}
+							qte = Integer.parseInt(tmp[2]);
+							
+							commandes.add(new Commande(client, plat, qte));
+							
+							break;
+						case "Fin":
+							// Fin de la lecture
+							break;
+					}
 				}
 			}
+			input.close();
+		}catch (FileNotFoundException e) {
+					e.printStackTrace();
 		}
-		input.close();
+	}
+	
+	
+	
+	/*
+	 * Sauvegarde et affichage des factures
+	*/
+	
+	/**
+	 * Permet de créer la facture du total des commandes d'un client
+	 * @param nomClient
+	 * 			Contient le nom du client courrant
+	 * @param cout
+	 * 			Contient la somme avant taxe des commandes du client courrant
+	 * @return
+	 * 		Une facture
+	 */
+	public static String creerFacture(String nomClient, double cout) {
+		double coutTPS = cout * 0.05;
+		double coutTVQ = cout * 0.10;
+		double total = cout + coutTPS + coutTVQ;
+		int nbE = 21 + String.format("%.2f",
+				Collections.max(Arrays.asList(cout, coutTPS, coutTVQ, total))).length();
+		String facture = ("Facture de " + nomClient + ":\r\n" + 
+				assemblerFacture(nbE, cout, "Av taxe:") +
+				assemblerFacture(nbE, coutTPS, "TPS(5%):") +
+				assemblerFacture(nbE, coutTVQ, "TVQ(10%):") +
+				assemblerFacture(nbE, total, "Total:"));
+		return facture;
+	}
+	/**
+	 * Defini le nombre d'espaces nécessaires pour ajuster l'affichage puis assemble la chaine
+	 * 
+	 * @param nbEspaces
+	 * 			Contient un nombre d'espaces calculé pour empêcher les débordements
+	 * @param cout
+	 * 			Contient le cout courrant
+	 * @param titre
+	 * 			Contient le titre qui défini le cout courrant
+	 * @return
+	 * 			Une ligne de la facture
+	 */
+	static String assemblerFacture(int nbEspaces, double cout, String titre) {
+		String sCout = String.format("%.2f", cout);
+		char[] espaces = new char[nbEspaces-(titre+sCout).length()];
+		Arrays.fill(espaces, ' ');
+		return (titre + new String(espaces) + sCout + "$\r\n");
 	}
 	/**
 	 * Création du fichier d'un fichier où les factures sont sauvegardées
 	 * @param nomFichier
 	 * 			Contient le nom du fichier
-	 * @throws FileNotFoundException
 	 */
-	public static void creerFacture(String nomFichier) throws FileNotFoundException {
-		PrintWriter sortie = new PrintWriter(nomFichier);
+	public static void creerFichierFacture(String nomFichier){
+		PrintWriter sortie;
+		try {
+			sortie = new PrintWriter(nomFichier);
 		sortie.println("Factures:");
 		for (Client cli : clients) {
 			double total = 0;
@@ -116,6 +163,10 @@ public class Main {
 			sortie.println(cli.nom + " " + total + "$");
 		}
 		sortie.close();
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 }
